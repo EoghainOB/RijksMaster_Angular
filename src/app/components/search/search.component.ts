8baf7081b68f30719999706902fa3f574bc7d365
Eoghain O'Briain <eoghain.obriain@appliedtechnology.se>
2023-06-16 14:35:03 +0200

    further tweaks and error handling

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Router } from '@angular/router';
 import { SearchService } from '../../service/search.service';
 import centuries from 'src/MockFiles/Centuries';
+import suggestions from 'src/MockFiles/ArtistList';
 
 @Component({
   selector: 'app-search',
@@ -10,30 +11,51 @@ import centuries from 'src/MockFiles/Centuries';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-  parseInt(arg0: any): number {
-    throw new Error('Method not implemented.');
-  }
   searchValue: string = '';
-  value: number = 0;
+  matchingSuggestions: string[] = [];
   page: number = 1;
   centuries = centuries;
+  suggestions = suggestions;
   century: number = 0;
 
   constructor(private router: Router, private searchService: SearchService) {}
+
+  handleInput(value: string) {
+    this.searchValue = value;
+    this.matchingSuggestions = this.getSuggestions(value);
+  }
+
+  handleInputFocus() {
+    this.matchingSuggestions = [];
+  }
+
+  getSuggestions(value: string): string[] {
+    return this.suggestions.filter((suggestion) =>
+      suggestion.toLowerCase().startsWith(value.toLowerCase())
+    );
+  }
+
+  handleSearch(suggestion: string) {
+    this.searchValue = suggestion;
+    this.search();
+  }
 
   search() {
     if (this.searchValue.length > 1) {
       this.searchService.setSearchTerm(this.searchValue);
       this.searchService.setSearch(this.searchValue);
+      this.searchService.setCentury(0);
       axios
         .get(
-          `https://www.rijksmuseum.nl/api/en/collection?key=6x1qSUeZ&q=${this.searchValue}&ps=10&p=${this.page}`
+          `https://www.rijksmuseum.nl/api/en/collection?key=6x1qSUeZ&q=${encodeURIComponent(
+            this.searchValue
+          )}&ps=10&p=${this.page}`
         )
         .then((res) => {
           if (res.data.artObjects) {
             this.searchService.setSearchData(res.data.artObjects);
-            this.searchService.setCentury(0);
             this.router.navigate(['/results']);
+            this.matchingSuggestions = [];
           } else {
             console.log('No results found');
           }
@@ -44,6 +66,8 @@ export class SearchComponent {
   dateSearch(event: any) {
     const century = parseInt(event);
     this.searchService.setCentury(century);
+    this.searchService.setSearchTerm('');
+    this.searchService.setSearch('');
     axios
       .get(
         `https://www.rijksmuseum.nl/api/en/collection?key=6x1qSUeZ&f.dating.period=${century}&ps=10&p=${this.page}`
@@ -51,8 +75,6 @@ export class SearchComponent {
       .then((res) => {
         if (res.data.artObjects) {
           this.searchService.setSearchData(res.data.artObjects);
-          this.searchService.setSearchTerm('');
-          this.searchService.setSearch('');
           this.router.navigate(['/results']);
         } else {
           console.log('No results found');
